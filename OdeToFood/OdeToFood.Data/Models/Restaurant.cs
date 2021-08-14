33c38a88a83a9351f2f46f99e5b84308c196c7e3
tohermann <tth4ik@gmail.com>
2021-08-15 02:24:48 +0300

    initial commit

---
+++ OdeToFood/OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name="Type of food")]
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,59 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pizza HATA", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Croissants", Cuisine = CuisineType.French},
+                new Restaurant { Id = 3, Name = "Pera i Peysy", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 4, Name = "Borsch i Galushky", Cuisine = CuisineType.Ukrainian},
+                new Restaurant { Id = 5, Name = "Bo Fo Food", Cuisine = CuisineType.Asian},
+            };
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(r => r.Id) + 1;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var existing = Get(restaurant.Id);
+            if(existing != null)
+            {
+                existing.Name = restaurant.Name;
+                existing.Cuisine = restaurant.Cuisine;
+            }
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = Get(id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+        }
+    }
+}
